6f6979f1a7383546a38b02e03b0efc8a27656846
jedwilshire <jedwilshire@gmail.com>
2021-12-07 12:33:45 -0600

    updated faq

--- Lesson-04-Using-Vectors/vector_controlled_projectile.py
+++ Lesson-04-Using-Vectors/vector_controlled_projectile.py
@@ -4,9 +4,11 @@ HEIGHT = 600
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
 class Circle_Sprite(pygame.sprite.Sprite):
-    def __init__(self, app, x, y):
+    def __init__(self, app, x, y, speed, angle):
         super().__init__()
         self.game = app
         self.image = pygame.Surface( (50, 50) )
@@ -14,8 +16,9 @@ class Circle_Sprite(pygame.sprite.Sprite):
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.pos = pygame.math.Vector2(x, y)
-        self.vel = pygame.math.Vector2(50, -50)
-        self.acc = pygame.math.Vector2(0, 20)
+        self.vel = pygame.math.Vector2(speed, 0)
+        self.vel = self.vel.rotate(angle)
+        self.acc = pygame.math.Vector2(0, 10)
         self.rect.center = self.pos
     
     def update(self):
@@ -28,10 +31,11 @@ class Application:
     def __init__(self):
         self.screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
         self.sprites = pygame.sprite.Group()
-        circle = Circle_Sprite(self, 100, 200)
-        self.sprites.add(circle)
+        self.circle = Circle_Sprite(self, 100, 200, 50, -45)
+        self.sprites.add(self.circle)
         self.running = True
         self.clock = pygame.time.Clock()
+        self.paused = False
         
     def gameloop(self):
         self.dt = self.clock.tick(60) / 1000
@@ -39,11 +43,19 @@ class Application:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                    self.paused = not self.paused
             # flip the display
             self.screen.fill(WHITE)
-            self.sprites.update()
+            if not self.paused:
+                self.sprites.update()
             self.sprites.draw(self.screen)
+            self.draw_vectors()
             pygame.display.flip()
+            
+    def draw_vectors(self):
+        pygame.draw.line(self.screen, RED, self.circle.pos, self.circle.pos + self.circle.vel)
+        pygame.draw.line(self.screen, GREEN, self.circle.pos, self.circle.pos + self.circle.acc)
 
 def main():
     pygame.init()
